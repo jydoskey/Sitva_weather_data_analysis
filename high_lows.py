d3f384aa8da8ced20c2f5001e0bb07b3208b2cbd
OMOTOLA OLAJIDE <jideomotola33@gmail.com>
2019-11-12 11:20:53 +0100

    line plot showing highs and lows for the year

--- high_lows.py
+++ high_lows.py
@@ -3,8 +3,8 @@ import csv
 from datetime import datetime
 from matplotlib import pyplot as plt
 
-#Get high temperatures from file
-filename = 'sitka_weather_07-2014.csv'
+#Get dates, high and low temperatures from file
+filename = 'sitka_weather_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -18,7 +18,7 @@ with open(filename) as f:
 
 #    -3-
     #Get dates and high temperature from file
-    dates, highs = [], []
+    dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[0], "%Y-%m-%d")
         dates.append(current_date)
@@ -26,20 +26,24 @@ with open(filename) as f:
         high = int(row[1])
         highs.append(high)
         
+        low = int(row[3])
+        lows.append(low)
+        
         #-3b- String format of the highest temperature
 #       highs.append(row[1])
         
 # Plot data
 fig = plt.figure(dpi=128, figsize=(10, 6))
-plt.plot(dates, highs, c='red')
+plt.plot(dates, highs, c='red', alpha=0.5)
+plt.plot(dates, lows, c='blue', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Format plot.
-plt.title("Daily high temperatures, July 2014", fontsize=24)
+plt.title("Daily high and low temperatures - 2014", fontsize=24)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
 
-plt.show()
-            
+plt.show()            
 #    print(highs)
